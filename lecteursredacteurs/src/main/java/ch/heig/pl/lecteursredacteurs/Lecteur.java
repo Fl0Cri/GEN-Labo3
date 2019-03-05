d001189dcb49127e63010289f69f4290f1dd6ed1
Haxos <a.mayocartes@gmail.com>
2019-03-05 17:29:04 +0100

    creating base methods and adding imports

--- lecteursredacteurs/src/main/java/ch/heig/pl/lecteursredacteurs/Lecteur.java
+++ lecteursredacteurs/src/main/java/ch/heig/pl/lecteursredacteurs/Lecteur.java
@@ -1,4 +1,17 @@
 package ch.heig.pl.lecteursredacteurs;
 
 public class Lecteur {
+    public Lecteur(Controleur controleur) {
+        
+    }
+
+    public void startRead() {
+    }
+
+    public boolean isWaiting() {
+        return false;
+    }
+
+    public void stopRead() {
+    }
 }
--- lecteursredacteurs/src/main/java/ch/heig/pl/lecteursredacteurs/Redacteur.java
+++ lecteursredacteurs/src/main/java/ch/heig/pl/lecteursredacteurs/Redacteur.java
@@ -1,4 +1,18 @@
 package ch.heig.pl.lecteursredacteurs;
 
 public class Redacteur {
+    public Redacteur(Controleur controleur) {
+
+    }
+
+    public boolean isWaiting() {
+
+        return false;
+    }
+
+    public void startWrite() {
+    }
+
+    public void stopWrite() {
+    }
 }
